4fd8c2d1f56dfd521fd9897cce71f7e8a4a14d6f
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-05-22 15:22:50 +0300

    update StekcitBMEmissions.sol

--- smart-contracts/StekcitBMEmissions.sol
+++ smart-contracts/StekcitBMEmissions.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-contract StekcitBwCEmissions {
+contract StekcitBMEmissions {
      event EvStekcitUser(
         uint256 id,
         address walletAddress,
